caca10d8a7c0c27be54e5747b1f344709acbf7dc
nomizo-iox <nomizo.iox@gmail.com>
2020-03-01 13:03:28 -0600

    Fixed the issue in Phonebook class by removing the mappedBy from the @ManyToMay.

--- src/main/java/io/nomizo/model/Customer.java
+++ src/main/java/io/nomizo/model/Customer.java
@@ -10,7 +10,6 @@ import java.util.Set;
 @Table(name = "customers")
 public class Customer {
 
-
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
@@ -18,13 +17,18 @@ public class Customer {
     private String lastName;
     private String email;
 
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "customer_phones")
+    private Set<PhoneBook> phoneBooks;
+
     public Customer() { super(); }
 
-    public Customer(long id, String firstName, String lastName, String email) {
+    public Customer(long id, String firstName, String lastName, String email, Set<PhoneBook> phoneBooks) {
         this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
+        this.phoneBooks = phoneBooks;
     }
 
     public long getId() { return id; }
@@ -38,6 +42,9 @@ public class Customer {
 
     public String getEmail() { return email; }
     public void setEmail(String email) { this.email = email; }
+
+    public Set<PhoneBook> getPhoneBooks() { return phoneBooks; }
+    public void setPhoneBooks(Set<PhoneBook> phoneBooks) { this.phoneBooks = phoneBooks; }
 
     @Override
     public String toString() {
