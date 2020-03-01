51dbb3612428083e5fb2cfbe619fe03776352a3c
nomizo-iox <nomizo.iox@gmail.com>
2020-02-29 23:48:22 -0600

    first commit

---
+++ src/main/java/io/nomizo/SpringBootCrudMigrationApplication.java
@@ -0,0 +1,13 @@
+package io.nomizo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCrudMigrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCrudMigrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/nomizo/SpringBootCrudMigrationApplicationTests.java
@@ -0,0 +1,13 @@
+package io.nomizo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCrudMigrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
